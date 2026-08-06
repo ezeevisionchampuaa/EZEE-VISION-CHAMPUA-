2a5861ef5c965944ab106b1064ca0c4b3d5cf7a9
ezeevisionchampuaa <ezeevisionchampua@gmail.com>
2026-08-06 05:44:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,3 +29,44 @@ function toggleTheme(){
 alert("Dark Mode Version 5.0 me aayega.");
 
 }
+function restoreBackup(event){
+
+const file = event.target.files[0];
+
+if(!file){
+
+return;
+
+}
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+
+try{
+
+const data = JSON.parse(e.target.result);
+
+localStorage.setItem(
+
+"students",
+
+JSON.stringify(data)
+
+);
+
+alert("Backup Restored Successfully ✅");
+
+location.href = "index.html";
+
+}catch(error){
+
+alert("Invalid Backup File ❌");
+
+}
+
+};
+
+reader.readAsText(file);
+
+}
