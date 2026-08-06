deee02d0ee24f5e45c7665c012fe657aa7dc9ccf
ezeevisionchampuaa <ezeevisionchampua@gmail.com>
2026-08-06 09:00:18 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,6 +5,9 @@ import {
 getFirestore
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+import { 
+getAuth 
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 const firebaseConfig = {
 
 apiKey: "AIzaSyAZ_XL6S9Z9BYPwiqnC_OGDMQrq4VODMoo",
@@ -22,7 +25,7 @@ appId: "1:87600683468:web:a4ae92fc6a8a0e4c88e569"
 };
 
 const app = initializeApp(firebaseConfig);
-
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { db };
+export { db, auth };
